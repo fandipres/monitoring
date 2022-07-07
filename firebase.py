f2653d2f656b747c368ed219cd864d000c08d15b
fandipres <fandi.simamora@gmail.com>
2022-07-07 18:42:20 +0700

    menambah fitur data kategori aktivitas

--- firebase.py
+++ firebase.py
@@ -15,53 +15,55 @@ kategori_ref=ref.child('Kategori_Aktivitas')
 riwayat_ref=ref.child('Riwayat_Monitoring')
 log_ref=ref.child('Log_Riwayat_Aktivitas')
 
-# Menu Versi 1
-# print("TAMBAH DATA")
-# print("1. Admin")
-# print("2. Member")
-# opsi = int(input("Opsi : "))
-# count = 0
-# if(opsi == 1):
-    # if(len(admin_ref.get())==0):
-        # count = 0
-    # else:
-        # snapshot = admin_ref.get(False, True)
-        # count = int([*snapshot][-1])
-        # count += 1
-    # print("TAMBAH DATA ADMIN")
-    # username = input("Username : ")
-    # password = input("Password : ")
-    # admin_ref.child(str(count)).set({'username': username, 'password': generate_password_hash(password, "sha256")})
-# elif(opsi == 2):
-    # if(len(member_ref.get())==0):
-        # count = 0
-    # else:
-        # snapshot = member_ref.get(False, True)
-        # count = int([*snapshot][-1])
-        # count += 1
-    # print("TAMBAH DATA MEMBER")
-    # username = input("Username : ")
-    # password = input("Password : ")
-    # member_ref.child(str(count)).set({'username': username, 'password': generate_password_hash(password, "sha256")})
-
-# Menu Versi 2
-print("TAMBAH DATA")
-print("1. Admin")
-print("2. Member")
+# Menu Utama
+print("MENU UTAMA")
+print("1. Menu User")
+print("2. Menu Aktivitas")
 opsi = int(input("Opsi : "))
+# Menu User
 if(opsi == 1):
-    print("TAMBAH DATA ADMIN")
-    username = input("Username : ")
-    password = input("Password : ")
-    if(admin_ref.child(username).get()==None):
-        admin_ref.child(username).set({'password': generate_password_hash(password, "sha256")})
-    else:
-        print("Username exists.")
+    print("MENU USER")
+    print("1. Lihat Data Admin")
+    print("2. Lihat Data Member")
+    print("3. Tambah Data Admin")
+    print("4. Tambah Data Member")
+    opsi = int(input("Opsi : "))
+    if(opsi == 1):
+        print("LIHAT DATA ADMIN")
+        tbl = admin_ref.get(True, False)[0].items() if member_ref.get()!=None else ""
+        print(tbl)
+    elif(opsi == 2):
+        print("LIHAT DATA MEMBER")
+        tbl = member_ref.get(True, False)[0].items() if member_ref.get()!=None else ""
+        print(tbl)
+    elif(opsi == 3):
+        print("TAMBAH DATA ADMIN")
+        username = input("Username : ")
+        password = input("Password : ")
+        if(admin_ref.child(username).get()==None):
+            admin_ref.child(username).set({'password': generate_password_hash(password, "sha256")})
+        else:
+            print("Username exists.")
+    elif(opsi == 4):
+        print("TAMBAH DATA MEMBER")
+        username = input("Username : ")
+        password = input("Password : ")
+        if(member_ref.child(username).get()==None):
+            member_ref.child(username).set({'password': generate_password_hash(password, "sha256")})
+        else:
+            print("Username exists.")
+# Menu Aktivitas
 elif(opsi == 2):
-    print("TAMBAH DATA MEMBER")
-    username = input("Username : ")
-    password = input("Password : ")
-    if(member_ref.child(username).get()==None):
-        member_ref.child(username).set({'password': generate_password_hash(password, "sha256")})
-    else:
-        print("Username exists.")
+    print("MENU AKTIVITAS")
+    print("1. Lihat Data Aktivitas")
+    print("2. Tambah Data Aktivitas")
+    opsi = int(input("Opsi : "))
+    if(opsi == 1):
+        print("LIHAT DATA AKTIVITAS")
+        tbl = kategori_ref.get(True, False)[0] if kategori_ref.get()!=None else ""
+        print(tbl)
+    elif(opsi == 2):
+        count = 0 if kategori_ref.get()==None else int([*kategori_ref.get(False, True)][-1])+1
+        print("TAMBAH DATA AKTIVITAS")
+        aktivitas = input("Nama Aktivitas : ")
+        kategori_ref.child(str(count)).set({'nama_aktivitas': aktivitas})
